27dc9bd8d84c6a78e2896b9372c6564e99f7d8da
Michael Brown <mbrown1413@gmail.com>
2015-09-06 08:04:25 -0400

    "Save As..." action; Ask if overwriting file

--- logic/interface.py
+++ logic/interface.py
@@ -1,6 +1,7 @@
 
-from collections import OrderedDict
+import os
 import json
+from collections import OrderedDict
 
 import gtk
 
@@ -9,8 +10,9 @@ import logic
 
 class Interface(gtk.Window):
 
-    def __init__(self, schematic=None):
+    def __init__(self, schematic=None, filename=None):
         super(Interface, self).__init__()
+        self.filename = filename
 
         if schematic is None:
             schematic = logic.Schematic()
@@ -19,9 +21,10 @@ class Interface(gtk.Window):
 
         menu_description = OrderedDict((
             ("File", OrderedDict((
-                ("New", lambda _: self.load_schematic(logic.Schematic())),
-                ("Open", lambda _: self.menu_load()),
+                ("New", lambda _: self.menu_new()),
+                ("Open", lambda _: self.menu_open()),
                 ("Save", lambda _: self.menu_save()),
+                ("Save As...", lambda _: self.menu_save_as()),
                 ("Exit", gtk.main_quit),
             ))),
             ("View", OrderedDict((
@@ -69,19 +72,6 @@ class Interface(gtk.Window):
         self.set_title("Logic Simulator")
         self.show_all()
 
-    def menu_load(self):
-        dialog = gtk.FileChooserDialog("Open Schematic...")
-        dialog.add_button("_Cancel", gtk.RESPONSE_CANCEL)
-        dialog.add_button("_Open", gtk.RESPONSE_OK)
-
-        try:
-            result = dialog.run()
-            if result == gtk.RESPONSE_OK:
-                filename = dialog.get_filename()
-                self.load_schematic_from_file(filename)
-        finally:
-            dialog.destroy()
-
     def load_schematic_from_file(self, filename):
         schematic = logic.Schematic.from_file(filename)
         self.load_schematic(schematic)
@@ -96,22 +86,70 @@ class Interface(gtk.Window):
         self.vbox.pack_start(self.schematic_widget, True, True, 0)
         self.show_all()
 
-    def menu_save(self):
-        dialog = gtk.FileChooserDialog("Save Schematic...", action=gtk.FILE_CHOOSER_ACTION_SAVE)
-        dialog.add_button("_Cancel", gtk.RESPONSE_CANCEL)
-        dialog.add_button("_Save", gtk.RESPONSE_OK)
-
-        try:
-            result = dialog.run()
-            if result == gtk.RESPONSE_OK:
-                filename = dialog.get_filename()
-                self.save_schematic(filename)
-        finally:
-            dialog.destroy()
-
     def save_schematic(self, filename):
         contents = json.dumps(self.schematic.get_dict(), indent=4)
         open(filename, "w").write(contents)
 
     def run(self):
         gtk.main()
+
+    def menu_new(self):
+        self.load_schematic(logic.Schematic())
+        self.filename = None
+
+    def menu_open(self):
+        dialog = gtk.FileChooserDialog("Open Schematic...")
+        dialog.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
+        dialog.add_button(gtk.STOCK_OPEN, gtk.RESPONSE_OK)
+
+        try:
+            result = dialog.run()
+            self.filename = dialog.get_filename()
+        finally:
+            dialog.destroy()
+
+        if result == gtk.RESPONSE_OK:
+            self.load_schematic_from_file(self.filename)
+
+    def menu_save(self):
+        if self.filename is None:
+            self.menu_save_as()
+            return
+
+        self.save_schematic(self.filename)
+
+    def menu_save_as(self):
+        dialog = gtk.FileChooserDialog("Save As...", action=gtk.FILE_CHOOSER_ACTION_SAVE)
+        dialog.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
+        dialog.add_button(gtk.STOCK_SAVE, gtk.RESPONSE_OK)
+
+        try:
+            result = dialog.run()
+            path = dialog.get_filename()
+        finally:
+            dialog.destroy()
+
+        if result == gtk.RESPONSE_OK:
+
+            if os.path.exists(path) and not self.dialog_confirm_save(path):
+                return
+
+            self.save_schematic(path)
+            self.filename = path
+
+    def dialog_confirm_save(self, filename):
+        dialog = gtk.MessageDialog(self,
+            gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+            gtk.MESSAGE_QUESTION,
+            gtk.BUTTONS_NONE,
+            'File "{}" already exists. Overwrite?'.format(filename)
+        )
+        dialog.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
+        dialog.add_button(gtk.STOCK_OK, gtk.RESPONSE_OK)
+
+        try:
+            response = dialog.run()
+        finally:
+            dialog.destroy()
+
+        return response == gtk.RESPONSE_OK
--- run.py
+++ run.py
@@ -7,13 +7,15 @@ import logic
 def main():
 
     if len(sys.argv) == 1:
+        filename = None
         schematic = logic.Schematic()
     elif len(sys.argv) == 2:
-        schematic = logic.Schematic.from_file(sys.argv[1])
+        filename = sys.argv[1]
+        schematic = logic.Schematic.from_file(filename)
     else:
         raise ValueError("Too many arguments")
 
-    interface = logic.Interface(schematic)
+    interface = logic.Interface(schematic, filename)
     interface.run()
 
 if __name__ == "__main__":
